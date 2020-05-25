bf9c843d56e44855483a4c1317f376bd2e4cf8f6
sandeep-AW <sandeep@ascenwork.com>
2020-05-25 20:32:38 +0530

    commited

---
+++ Wizrr/WizrrWeb/Models/AssetManagement/AM_StatusMasterModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WizrrWeb.Models.AssetManagement
+{
+    public class AM_StatusMasterModel
+    {
+        public int ID { get; set; }
+
+        public string StatusName { get; set; }
+    }
+}
